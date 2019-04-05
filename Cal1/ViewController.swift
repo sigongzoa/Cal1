661db1defe900f6aa04f7f701806fdb5841af13b
KyungKoo Kang <kgkg0308@sogang.ac.kr>
2019-04-05 09:03:03 -0700

    Initial Commit

---
+++ Cal1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cal1
+//
+//  Created by sgcs on 2019. 4. 5..
+//  Copyright © 2019년 KyungKoo Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
